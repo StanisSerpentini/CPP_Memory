a57135f46fd8b4b00a0ef676b3aa1d3d109bd00f
Stanis Serpentini <stanis.serpentini@epitech.eu>
2026-09-15 15:46:00 +0200

    UPDATE help message

--- src/help.cpp
+++ src/help.cpp
@@ -6,8 +6,11 @@ void printHelp() {
         << "\t-h\tShow help message and quit." << std::endl
         << "\t-s N\tUse a board of size N directly instead of asking for the board size." << std::endl
         << "Rules:" << std::endl
-        << "Click on two cells to show their value." << std::endl
+        << "Select two cells to show their value." << std::endl
         << "If the two cells have the same value, they will remain face-up; "
         << "otherwise, they flip back to being face-down." << std::endl
-        << "The goal is to find all the pairs." << std::endl;
+        << "The goal is to find all the pairs." << std::endl
+        << "Controls:" << std::endl
+        << "Use z,q,s and d to move the cursor and space to select a cell." << std::endl
+        << "Use x to quit the game." << std::endl;
 }
